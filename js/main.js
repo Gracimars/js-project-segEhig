4d61737fc444d6061025a3ac34f8e17bab602243
Gracimar Perez Morgado <gracipm92@gmail.com>
2023-03-31 22:09:20 -0300

    version in production

--- js/main.js
+++ js/main.js
@@ -1,43 +1,77 @@
-function init(e) {
+const dataPrices = {
+	typesOfEvaluations: [
+		{type: "iluminacion", cost: 1500},
+		{type: "ruido", cost: 1700},
+		{type: "ergonomia", cost: 2000},
+	],
+	ubicationCompany: [
+		{
+			ubication: "caba",
+			prices: {
+				priceA: 200,
+				priceB: 450,
+				priceC: 900,
+			},
+		},
+		{
+			ubication: "gran buenos aires",
+			prices: {
+				priceA: 300,
+				priceB: 550,
+				priceC: 1000,
+			},
+		},
+	],
+};
+async function init(e) {
 	e.preventDefault();
+	const dolarBlue = await fetch("https://criptoya.com/api/dolar")
+		.then((res) => res.json())
+		.then((res) => {
+			return res.oficial;
+		});
 	let ubicationCompany;
 	let typeOfEvaluation;
 	let numberOfJobs = document.getElementById("numberOfJobs").value;
 	if (numberOfJobs) {
-		ubicationCompany = document.getElementById("ubicationCompany").value.toLowerCase();
+		ubicationCompany = document.getElementById("ubicationCompany").value;
 	}
 	if (ubicationCompany) {
-		typeOfEvaluation = document.getElementById("ubicationCompany").value;
+		typeOfEvaluation = document.getElementById("typeOfEvaluation").value;
 	}
-	const total = calcCostTotal(numberOfJobs, ubicationCompany, typeOfEvaluation);
+	const total =
+		calcCostTotal(dataPrices, numberOfJobs, ubicationCompany, typeOfEvaluation) * dolarBlue;
 	if (total) {
-		console.log(total);
+		Swal.fire({
+			icon: "success",
+			text: `Gracias por completar el presupuesto, a la brevedad se comunicara un asistente. Su total es $${total}`,
+		});
 	} else {
 		console.log("Por favor ingrese los datos.");
 	}
 }
 
-function calcPositionAndUbication(numberOfJobs, ubicationCompany) {
+function calcPositionAndUbication(data, numberOfJobs, ubicationCompany) {
 	let resultado;
-	while (ubicationCompany == "caba") {
+	while (ubicationCompany == data.ubicationCompany[0].ubication) {
 		if (numberOfJobs <= 5) {
-			resultado = 200;
+			resultado = data.ubicationCompany[0].prices.priceA;
 		} else if (numberOfJobs > 5 && numberOfJobs <= 10) {
-			resultado = 450;
+			resultado = data.ubicationCompany[0].prices.priceB;
 		} else if (numberOfJobs > 10 && numberOfJobs <= 20) {
-			resultado = 900;
+			resultado = data.ubicationCompany[0].prices.priceC;
 		} else {
 			resultado = "Lo siento, mas de 20 puestos excede nuestro limite actual de evaluaciones";
 		}
 		return resultado;
 	}
-	while (ubicationCompany == "gran buenos aires") {
+	while (ubicationCompany == data.ubicationCompany[1].ubication) {
 		if (numberOfJobs <= 5) {
-			resultado = 300;
+			resultado = data.ubicationCompany[1].prices.priceA;
 		} else if (numberOfJobs > 5 && numberOfJobs <= 10) {
-			resultado = 550;
+			resultado = data.ubicationCompany[1].prices.priceB;
 		} else if (numberOfJobs > 10 && numberOfJobs <= 20) {
-			resultado = 1000;
+			resultado = data.ubicationCompany[1].prices.priceC;
 		} else {
 			resultado = "Lo siento, mas de 20 puestos excede nuestro limite actual de evaluaciones";
 		}
@@ -45,19 +79,19 @@ function calcPositionAndUbication(numberOfJobs, ubicationCompany) {
 	}
 }
 
-function calcCostTotal(numberOfJobs, ubicationCompany, typeOfEvaluation) {
-	let costo = calcPositionAndUbication(numberOfJobs, ubicationCompany);
+function calcCostTotal(data, numberOfJobs, ubicationCompany, typeOfEvaluation) {
+	let costo = calcPositionAndUbication(data, numberOfJobs, ubicationCompany);
 	let resultado;
 
 	if (typeOfEvaluation) {
 		switch (typeOfEvaluation) {
-			case "evaluacion ergonomica":
+			case "ergonomia":
 				resultado = costo + 2000;
 				break;
-			case "evaluacion de iluminacion":
+			case "iluminacion":
 				resultado = costo + 1500;
 				break;
-			case "evaluacion de ruido":
+			case "ruido":
 				resultado = costo + 1700;
 				break;
 			default:
@@ -67,3 +101,28 @@ function calcCostTotal(numberOfJobs, ubicationCompany, typeOfEvaluation) {
 	return resultado;
 }
 document.getElementById("formulary").addEventListener("submit", init);
+
+//LOCAL STORAGE & JSON
+// Guardar datos
+localStorage.setItem("ruidoEvaluation", "ruido");
+localStorage.setItem("iluminacionEvaluation", "iluminacion");
+localStorage.setItem("evaluacionErgonomica", "ergonomia");
+
+// Obtener datos
+let prueba = localStorage.getItem("ruidoEvaluation");
+console.log(prueba);
+
+// Modificar datos
+localStorage.setItem("ruidoEvaluation", "sonidos");
+
+// Borrar datos
+localStorage.removeItem("ruidoEvaluation");
+
+//CRUD --> Create Read Update Delete
+//sessionStorage.setItem("fecha", new Date());
+
+for (let i = 0; i < localStorage.length; i += 1) {
+	let clave = localStorage.key(i); // CLAVES
+	console.log("Clave: " + clave);
+	console.log("Valor: " + localStorage.getItem(clave));
+}
